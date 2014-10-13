3b7c552d1f3d27d1bc10e76314d589eccd148cfe
adamrob <adamrob@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-13 11:33:26 +0000

    V0.2 + Initial Wordpress Release
    git-svn-id: https://plugins.svn.wordpress.org/adamrob-parallax-scroll/trunk@1006525 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ adamrob-parallax_scroll.php
@@ -0,0 +1,67 @@
+<?php
+/**
+* Plugin Name: Parallax Scroll by Adamrob.co.uk
+* Plugin URI: http://www.adamrob.co.uk
+* Description: Easily create a page header or even a post with a parallax scrolling background image, with just a shortcode! Visit adamrob.co.uk for more information and support.
+* Version: 0.2
+* Author: adamrob
+* Author URI: http://www.adamrob.co.uk
+* License: A "Slug" license name e.g. GPL12
+*/
+
+/*  Copyright 2014 adamrob (www.adamrob.co.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+	PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+	HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+	OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+
+/********************************
+** adamrob.co.uk - 2OCT2014
+** Parallax Scroll Wordpress Plugin
+** Uses Parallax.js (https://github.com/pixelcog/parallax.js)
+**
+** For Help and Support please visit www.adamrob.co.uk
+**
+** Main Plugin Call
+********************************/
+
+
+//Define Variables
+define('PARALLAX_PATH', dirname(__FILE__) . "/");
+define('PARALLAX_MINHEIGHT', 100);
+define('PARALLAX_DEFVPOS', "middle");
+define('PARALLAX_DEFHPOS', "center");
+define('PARALLAX_POSTTYPE', "parallax_scroll");
+define('PARALLAX_SHORTCODE', "parallax-scroll");
+
+
+//Include external Scripts
+wp_register_style( 'parallax-CSS', plugins_url( '/css/parallax.css', __FILE__ ) );
+wp_register_script( 'parallax-script', plugins_url( '/includes/parallax/parallax.js', __FILE__ ) );
+
+//Include external files
+require(PARALLAX_PATH . 'includes/adamrob-parralax-post-ops.php');
+require(PARALLAX_PATH . 'includes/adamrob-parralax-post-type.php');
+require(PARALLAX_PATH . 'includes/adamrob-parralax-shortcode.php');
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,55 @@
+=== Parallax Scroll ===
+Contributors: adamrob
+Tags: parallax, scroll, image, header, adamrob
+Requires at least: 4.0
+Tested up to: 4.0
+Stable tag: 0.2
+License: GPL2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Create a header, or even a custom post/page with a scrolling parallax background. All with a simple shortcode.
+
+== Description ==
+Parallax Scroll; the easiest way to get a parallax scrolling background image for an element on your page/posts.
+
+Examples include:
+* Create a page/post header text with a parallax scrolling background. Ideal for single page sites, or pages with sections.
+* Create a full page/post with a parallax scrolling background. Ideal for single page sites, or pages with sections where you can have more than one page/post.
+* Give single elements of your pages a parallax scrolling background.
+
+How?
+* Simply create the content required in the custom Parallax Scroll post type.
+* The Parallax Scroll admin page will display the shortcode required for all Parallax Scroll post types defined. Copy this shortcode, and paste it into any page or post.
+* Thats it!
+
+Parallax Scroll uses parallax.js for the parallax effect. For more information please see source files.
+
+== Installation ==
+No special steps required for installation.
+
+Auto Install.
+1. Navigate to plugins page in your WordPress admin section.
+1. Search for Parallax Scroll by adamrob.co.uk.
+1. Download and activate.
+1. You will now have the Parallax Scroll menu item in your WordPress admin
+
+Manual Install.
+1. Download the plugin files.
+1. Copy/upload the plugin folder to your WordPress Content/plugins directory.
+1. Navigate to WordPress plugin page in your WordPress admin page. The plugin will be displayed.
+1. Click activate. You will now have the Parallax Scroll menu item in your WordPress admin.
+
+== Frequently Asked Questions ==
+
+= Where can i get support? =
+
+Please visit [adamrob.co.uk](http://adamrob.co.uk/parallax-scroll "my website") for support and/or suggestions.
+
+
+== Changelog ==
+
+= 0.1 =
+* Initial release.
+
+= 0.2 =
+* Added header CSS style parameter to custom post type.
