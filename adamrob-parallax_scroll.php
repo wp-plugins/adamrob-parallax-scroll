7ea2a7fbcf7b3cbcc30d6507ab7d5a5ee40b66d6
adamrob <adamrob@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-14 13:37:16 +0000

    changed version number
    git-svn-id: https://plugins.svn.wordpress.org/adamrob-parallax-scroll/trunk@1007288 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- adamrob-parallax_scroll.php
+++ adamrob-parallax_scroll.php
@@ -3,7 +3,7 @@
 * Plugin Name: Parallax Scroll by Adamrob.co.uk
 * Plugin URI: http://www.adamrob.co.uk/parallax-scroll
 * Description: Easily create a page header or even a post with a parallax scrolling background image, with just a shortcode! Visit adamrob.co.uk for more information and support.
-* Version: 0.2
+* Version: 0.3
 * Author: adamrob
 * Author URI: http://www.adamrob.co.uk
 * License: A "Slug" license name e.g. GPL12
